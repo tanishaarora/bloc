f1295f12a2d60dc86b40de9955e6a317a794ddf0
Tanisha Arora <Tanisha.Arora@SRMTECHSOL.Local>
2023-09-07 18:24:37 +0530

    Initial Commit

---
+++ lib/features/cart/card_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'card_event.dart';
+part 'card_state.dart';
+
+class CardBloc extends Bloc<CardEvent, CardState> {
+  CardBloc() : super(CardInitial()) {
+    on<CardEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/features/cart/card_state.dart
@@ -0,0 +1,6 @@
+part of 'card_bloc.dart';
+
+@immutable
+abstract class CardState {}
+
+class CardInitial extends CardState {}
